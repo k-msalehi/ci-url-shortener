0bb26efa51a5b4d36cbec5630f725af8a0b6b7dd
MohammadSalehi <mds1121@yahoo.com>
2022-02-22 11:46:24 +0330

    add GroceryCrud

--- app/Controllers/Admin/Redirect.php
+++ app/Controllers/Admin/Redirect.php
@@ -3,11 +3,17 @@
 namespace App\Controllers\Admin;
 
 use App\Controllers\BaseController;
+use App\Libraries\GroceryCrud;
 
 class Redirect extends BaseController
 {
     public function index()
     {
-        
+        $crud = new GroceryCrud();
+        $crud->setTable('redirects');
+
+        $output = $crud->render();
+        return view('admin.redirect.index');
+        return $this->_exampleOutput($output);
     }
 }
--- app/Controllers/Redirect.php
+++ app/Controllers/Redirect.php
@@ -4,13 +4,21 @@ namespace App\Controllers;
 
 use App\Controllers\BaseController;
 use App\Models\RedirectModel;
+use CodeIgniter\Exceptions\PageNotFoundException;
 
 class Redirect extends BaseController
 {
-    public function redirect($from='')
+    public function index()
+    {
+        dd('stop');
+        return redirect()->to(MAIN_URL);
+    }
+    public function redirect($from = '')
     {
         $redirect = model(RedirectModel::class);
-        $redirect = $redirect->where('from',$from)->findAll(1);
-        return redirect()->to($redirect[0]['to']);
+        $redirect = $redirect->where('from', $from)->findAll(1);
+        if ($redirect)
+            return redirect()->to($redirect[0]['to']);
+        throw PageNotFoundException::forPageNotFound('URL not exist.');
     }
 }
