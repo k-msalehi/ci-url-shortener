300adc5067d166b688e17364ea27d194ebd9e52b
MohammadSalehi <mds1121@yahoo.com>
2022-02-23 15:57:21 +0330

    update

--- app/Controllers/Redirect.php
+++ app/Controllers/Redirect.php
@@ -4,6 +4,7 @@ namespace App\Controllers;
 
 use App\Controllers\BaseController;
 use App\Models\RedirectModel;
+use App\Models\VisitModel;
 use CodeIgniter\Exceptions\PageNotFoundException;
 
 class Redirect extends BaseController
@@ -16,8 +17,48 @@ class Redirect extends BaseController
     {
         $redirect = model(RedirectModel::class);
         $redirect = $redirect->where('from', $from)->first();
-        if ($redirect)
+        if ($redirect) {
+            $this->newVisit($redirect['from']);
             return redirect()->to($redirect['to']);
+        }
         throw PageNotFoundException::forPageNotFound('URL not exist.');
+    }
+
+    private function newVisit($from)
+    {
+        helper('cookie');
+        $agent = $this->request->getUserAgent();
+
+        $browser = $agent->getBrowser() . ' ' . $agent->getVersion();
+        if ($agent->isRobot()) {
+            $browser = $agent->getRobot();
+        }
+        $os = $agent->getPlatform();
+        $referrer  = $agent->getReferrer();
+
+        $visit = model(VisitModel::class);
+        $redirect = model(RedirectModel::class);
+
+        $redirect = $redirect->where('from', $from)->first();
+        $uid = uniqid('', true);
+
+        //check if user already visited this link
+        if (get_cookie('user_id')) {
+            $uid = get_cookie('user_id');
+        } else {
+            setcookie('user_id', $uid, time() + (86400 * 365));
+        }
+        if (!get_cookie($from)) {
+            setcookie($from, 'yes', time() + (86400 * 365));
+        }
+        //dd($uid);
+        $data = [
+            'redirect_id' => $redirect['id'],
+            'user_id' => $uid,
+            'referrer' => $referrer,
+            'os' => $os,
+            'browser' => $browser,
+        ];
+        $visit->insert($data);
     }
 }
